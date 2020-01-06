f6a874e919ad5e4f84d224e8092a54aa937d0609
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-01-06 12:45:51 -0500

    data finally pulls from api,

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -12,33 +12,39 @@ class Main extends Component {
   componentDidMount() {
     const url =
       "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=63f2a4bf5bdd42b3bf1f0851aa78b335";
-    console.log(url);
+
     fetch(url)
       .then(res => res.json())
       .then(res => {
-          //PULLS DATA FROM HERE TO CONSOLE. 
+        //PULLS DATA FROM HERE TO CONSOLE.
         console.log(res);
 
         this.setState({
-          newsdata: res
+          newsdata: res.articles
         });
       });
   }
 
-  render() {
-    return (
 
+  render() {
+   
+    return (
       <div className="news-articles">
+        <>
         <h1>Articles from the Web!</h1>
-        {this.state.newsdata.map((articles) => (
-          <div>
-            <h2>{articles.author}</h2>
-            {/* <h3>{articles.description}</h3>
-            <img src={articles.urlToImage} /> */}
-          </div>
-        ))}
+       {this.state.newsdata.map(articles => (
+         <ul>
+       
+       <li>Title: {articles.title}</li>
+  <li>Source: {articles.source.name}</li>
+       <img src={articles.urlToImage} />
+  <li>Content: {articles.content}</li>
+       </ul>
+       ))}
+   
+       </>
       </div>
-      )
+    );
   }
 }
 
