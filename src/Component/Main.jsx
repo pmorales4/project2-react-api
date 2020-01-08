cff78ed9caa16d42fff73ae379788ffc453bb412
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-01-08 10:35:53 -0500

    somewhat fixed the divs side by side

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
 import BreakingNews from "../Component/BreakNews/BreakingNews.jsx";
-import Title from '../Component/BreakNews/BreakingNewsTitle.jsx';
-import EnterTitle from '../Component/EntertainNews/EntertainmentNewsTitle.jsx';
+import Title from "../Component/BreakNews/BreakingNewsTitle.jsx";
+import EnterTitle from "../Component/EntertainNews/EntertainmentNewsTitle.jsx";
 import EntertainNews from "../Component/EntertainNews/EntertainmentNews.jsx";
-
-
-
-
 
 class Main extends Component {
   constructor() {
@@ -19,7 +15,6 @@ class Main extends Component {
   }
 
   componentDidMount() {
-
     // BREAKING NEWS TOP HEADLINE NEWS DATA PULL BELOW
     const url =
       "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=63f2a4bf5bdd42b3bf1f0851aa78b335";
@@ -27,43 +22,37 @@ class Main extends Component {
     fetch(url)
       .then(res => res.json())
       .then(res => {
-       
         this.setState({
           newsdata: res.articles
         });
       });
 
+    // ENTERTAINMENT NEWS DATA PULL BELOW
 
-      // ENTERTAINMENT NEWS DATA PULL BELOW
-
-    fetch('https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=63f2a4bf5bdd42b3bf1f0851aa78b335')
+    fetch(
+      "https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=63f2a4bf5bdd42b3bf1f0851aa78b335"
+    )
       .then(res => res.json())
       .then(res => {
-       
         this.setState({
           entertainmentdata: res.articles
         });
       });
-
   }
-
-
-
-
-
-
 
   render() {
     return (
       <div className="news-articles">
         <>
-          <Title />
-          <div className="breakingnews-section-div">
-          <BreakingNews data={this.state.newsdata}/>
-          </div>
-          <EnterTitle />
-          <div className="entertainnews-section-div">
-          <EntertainNews data={this.state.entertainmentdata}/>
+          <div className="top-article-section">
+            <div className="breakingnews-section-div">
+              <Title />
+              <BreakingNews data={this.state.newsdata} />
+            </div>
+            <div className="entertainnews-section-div">
+              <EnterTitle />
+              <EntertainNews data={this.state.entertainmentdata} />
+            </div>
           </div>
         </>
       </div>
